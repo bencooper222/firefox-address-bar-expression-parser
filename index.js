c42a9ac57484ce2a09460b51e1d50ddfce56f277
Ben Cooper <benjamin.cooper@convoy.com>
2021-05-28 13:40:44 -0700

    Not a module

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ browser.omnibox.setDefaultSuggestion({
   description: `Do most mathematical computations.`,
 });
 
-
 browser.omnibox.onInputChanged.addListener((text, suggest) => {
   const data = evaluate(text);
   suggest([{ description: data, content: data }]);
