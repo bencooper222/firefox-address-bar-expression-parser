a8ceaa0482bf27225a060f8a7338ba7b38768429
Ben Cooper <benjamin.cooper@convoy.com>
2021-09-21 20:33:07 -0700

    add extern

---
+++ index.extern.js
@@ -0,0 +1,9 @@
+/**
+ * @fileoverview Clarifying the browser global is available.
+ * @externs
+ */
+
+const browser = {};
+
+browser.omnibox.onInputStarted.addListener = function (cb) {};
+browser.omnibox.onInputChanged.addListener = function (cb) {};
