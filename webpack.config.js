a57a4a393aee256b8b575a8fe4140633a2986c48
Ben Cooper <benjamin.cooper@convoy.com>
2021-09-21 20:21:32 -0700

    first attempt at adding closure plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,14 @@
-const path = require('path');
+const path = require("path");
+const ClosurePlugin = require('closure-webpack-plugin');
 
 module.exports = {
+  optimization: { minimizer: [new ClosurePlugin({ mode: "STANDARD" }, {})] },
   entry: {
-    addon: './index.js',
+    addon: "./index.js",
   },
   output: {
-    path: path.resolve(__dirname, './'),
-    filename: '[name]/index.js',
+    path: path.resolve(__dirname, "./"),
+    filename: "[name]/index.js",
   },
   optimization: { usedExports: true },
 };
